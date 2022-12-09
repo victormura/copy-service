5ced9e59b91deb249527a2ae841741f4edf2c3e5
Victor Mura <victormura.s@gmail.com>
2022-12-09 16:02:55 +0000

    Unlock job progress mutex on cancel if state was paused

--- lib/cancel.c
+++ lib/cancel.c
@@ -8,6 +8,7 @@ int copy_cancel(copyjob_t job)
     pthread_mutex_lock(&job_stats_mutexes[job]);
     close(jobs_stats[job].dst_fd);
     close(jobs_stats[job].src_fd);
+    int old_state = jobs_stats[job].state;
     jobs_stats[job].state = CANCELED;
     if (unlink(jobs_stats[job].dst))
     {
@@ -15,6 +16,7 @@ int copy_cancel(copyjob_t job)
         return -1;
     }
     pthread_mutex_unlock(&job_stats_mutexes[job]);
+    if (old_state == PAUSED) pthread_mutex_unlock(&job_mutexes[job]);
     sem_post(&semaphore);
     return 0;
 };
